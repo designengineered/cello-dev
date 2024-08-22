91acb79f4ab648b77824ed6a5612bc2d2ac15634
laztaxon <cello@cello.design>
2024-08-22 18:29:06 -0400

    adding svelte 2024-08-22 6:29 PM

---
+++ src/components/svelte/TestComponent.svelte
@@ -0,0 +1,34 @@
+<script>
+	import Nested from "./Nested.svelte";
+</script>
+
+<div class="container">
+	<Nested answer={42} />
+</div>
+
+<style>
+	@import "src/styles/colors.css";
+	/* .alert { */
+	/* 	font-size: 2rem; */
+	/* 	font-weight: 800; */
+	/* 	color: var(--color-accent); */
+	/* } */
+	.container {
+		display: flex;
+		flex-direction: column-reverse;
+		align-items: center;
+		color: var(--color-text);
+	}
+	/* p { */
+	/* 	font-weight: 500; */
+	/* 	font-size: 1.5rem; */
+	/* } */
+	/* button { */
+	/* 	background: var(--color-accent); */
+	/* 	font-size: 1.5rem; */
+	/* 	font-weight: 800; */
+	/* 	color: var(--color-background); */
+	/* 	border-radius: 8px; */
+	/* 	padding-block: 0.5rem; */
+	/* } */
+</style>
